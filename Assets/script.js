712037db86da9de9cf87f2fb2c809d72fef00f46
C.T. Bell <charlestbell@gmail.com>
2020-12-18 21:36:43 -0500

    Got the api responding

--- Assets/script.js
+++ Assets/script.js
@@ -6,32 +6,47 @@
 //Api that gets the UV index (using the lat long)
 //
 
-var cityName = "";
-let queryUrlLatLong = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=abf76a69e67fc664083162f1310bc36e`;
+var cityName;
 
-let queryUrlForcast =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&appid=abf76a69e67fc664083162f1310bc36e";
-
-function getWeather() {
-  console.log(cityName);
-  $.ajax({
-    url: queryUrlLatLong,
-    method: "GET",
-  }).then(function (responseLatLong) {
-    console.log(responseLatLong);
-    $.ajax({
-      url: queryUrlForcast,
-      method: "GET",
-    }).then(function (responseForcast) {
-      let latitude = response.city.coord.lat;
-      console.log(responseForcast);
-      console.log(latitude);
-    });
-  });
-}
+// $.ajax({
+//   url: queryUrlLatLong,
+//   method: "GET",
+// }).then(function (responseLatLong) {
+//   console.log(responseLatLong);
+//   $.ajax({
+//     url: queryUrlForcast,
+//     method: "GET",
+//   }).then(function (responseForcast) {
+//     let latitude = response.city.coord.lat;
+//     console.log(responseForcast);
+//     console.log(latitude);
+//   });
+// });
 
 $("#searchBtn").on("click", function (event) {
   event.preventDefault();
-  cityName = $("#cityInput").val();
-  getWeather();
+  if ($("#cityInput").val() !== "") {
+    cityName = $("#cityInput").val();
+    console.log(cityName);
+    let queryUrlLatLong = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=abf76a69e67fc664083162f1310bc36e`;
+    //Get Weather
+    $.ajax({
+      url: queryUrlLatLong,
+      method: "GET",
+    }).then(function (responseLatLong) {
+      console.log(responseLatLong);
+      var latitude = responseLatLong.city.coord.lat;
+      var longitude = responseLatLong.city.coord.lon;
+      var queryUrlForcast = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=abf76a69e67fc664083162f1310bc36e`;
+
+      $.ajax({
+        url: queryUrlForcast,
+        method: "GET",
+      }).then(function (responseForcast) {
+        console.log(responseForcast);
+        $("#cityTitleMain").text(event.city.name);
+      });
+    });
+    //write the weathe to the screen
+  }
 });
